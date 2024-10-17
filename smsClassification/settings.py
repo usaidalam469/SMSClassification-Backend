bf39632734a04b093fe11916cfe23694160549ba
Usaid <usaidalam469@gmail.com>
2024-10-17 17:22:23 +0100

    Minor changes

--- smsClassification/settings.py
+++ smsClassification/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cf9h%=-f1@q01=_t!rs@mpa*4a61ns=)&*lc6hw^8$fkhho-$d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
