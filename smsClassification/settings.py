9ac1550c2f3f0cd363261fab7b0e3b662894545c
Usaid <usaidalam469@gmail.com>
2024-10-17 16:58:27 +0100

    Minor changes

--- smsClassification/settings.py
+++ smsClassification/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATiC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 django_heroku.settings(locals())
